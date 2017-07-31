0ca996fd9ec0ba5e79a748eabea5f516b86282c2
Sebastian Gogoasa <sebastian.gogoasa@sinwise.com>
2017-07-31 13:29:26 +0100

    OS Testing...

--- include/mysafesqlCheck.py
+++ include/mysafesqlCheck.py
@@ -13,7 +13,6 @@ import imp, sys, os, platform
 sys.path.append('')
 from mysafesqlColors import *
 
-supported_linux_oses    = ["CentOS", "Ubuntu"]
 installed_packages      = ["mysqlpump", "pigz"]
 
 def CheckSystem():
@@ -21,43 +20,25 @@ def CheckSystem():
     if sys.platform.startswith('linux'):
         distro, version, dist_id = platform.linux_distribution()
         if "CentOS" in distro:
-            printGreen("CentOS found...")
-        elif distro in ('Debian', 'debian'):
-            printGreen("Debian found...")
-        elif distro == 'Fedora':
-            printGreen("Fedora found...")
-        elif distro == 'Gentoo Base System':
-            printGreen("Gentoo found...")
-        elif distro in ('Mint', 'LinuxMint'):
-            printGreen("Mint found...")
-        elif distro == 'Ubuntu':
-            printGreen("Ubuntu found...")
+            isCentOS = True
+            checkDependencies()
+        elif "Ubuntu" in distro:
+            checkDependencies()
+            isUbuntu = True
         else:
             printRed("Aborting, your operating system is not currenly supported.")
-            print distro
     elif sys.platform.startswith('darwin'):
         printRed("Mac OS is not currenly supported")
+        exit()
     elif sys.platform.startswith('openbsd'):
         printRed("OpenBSD is not currenly supported")
+        exit()
 
-    # try:
-    #     with open('/etc/redhat-release', 'rb', 0) as file, mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-    #         if s.find(b'CentOS') == -1:
-    #             isCentOS = True
-    #         if s.find(b'Ubuntu') == -1:
-    #             isUbuntu
-    #         if s.find(b'CentOS') != -1 or s.find(b'Ubuntu'):
-    #             isNotSupported = True
-    # except:
-    #     printRed("Aborting, opertaing system not supported!")
-    #     exit()
+def checkDependencies():
 
-    # if isNotSupported:
-    #     printRed("Aborting, opertaing system not supported!")
-    #     exit()
+    if isCentOS:
+        printGreen("CentOS found on your server, checking packages...")
 
-    # if isCentOS:
-    #     printGreen("CentOS found...")
 
-    # if isUbuntu:
-    #     printGreen("Ubuntu found...")
+    if isUbuntu:
+        printGreen("Ubuntu found on your server, checking packages...")
