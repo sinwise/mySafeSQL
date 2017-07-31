19b100153bca15e6ad1c24fcec6ea5655571f97d
Sebastian Gogoasa <sebastian.gogoasa@sinwise.com>
2017-07-31 13:12:52 +0100

    OS Testing...

--- include/mysafesqlCheck.py
+++ include/mysafesqlCheck.py
@@ -13,12 +13,40 @@ import imp, sys, os, platform
 sys.path.append('')
 from mysafesqlColors import *
 
+supported_linux_oses    = ["CentOS", "Ubuntu"]
+installed_packages      = ["mysqlpump", "pigz"]
 
 def CheckSystem():
 
-    print(os.name)
-    print(platform.system())
-    print(platform.release())
+    if sys.platform.startswith('linux'):
+        distro, version, dist_id = platform.linux_distribution()
+        if distro == 'CentOS':
+            printGreen("CentOS found...")
+        elif distro in ('Debian', 'debian'):
+            printGreen("Debian found...")
+        elif distro == 'Fedora':
+            printGreen("Fedora found...")
+        elif distro == 'Gentoo Base System':
+            printGreen("Gentoo found...")
+        elif distro in ('Mint', 'LinuxMint'):
+            printGreen("Mint found...")
+        elif distro == 'Ubuntu':
+            printGreen("Ubuntu found...")
+        else:
+            printRed("Aborting, your operating system is not currenly supported.")
+
+        args['version'] = version
+        args['dist_id'] = dist_id
+
+    elif sys.platform.startswith('darwin'):
+            # TODO Support Darwin platforms that aren't OS X.
+            #osx_version = platform.mac_ver()[0]
+        printRed("Mac OS is not currenly supported")
+
+    elif sys.platform.startswith('openbsd'):
+            # cls = OpenBSDBootstrapper
+            # args['version'] = platform.uname()[2]
+        printRed("OpenBSD is not currenly supported")
 
     # try:
     #     with open('/etc/redhat-release', 'rb', 0) as file, mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
