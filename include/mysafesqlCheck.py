3df8f17710c960b1e4d8cd30a42fc3ad52eefa67
Sebastian Gogoasa <sebastian.gogoasa@sinwise.com>
2017-07-31 10:51:49 +0100

    Testing OS type...

--- include/mysafesqlCheck.py
+++ include/mysafesqlCheck.py
@@ -20,30 +20,48 @@
     # else:
     #     print "not installed"
 
-import mmap, imp, sys
+import imp, sys, os, platform
 sys.path.append('')
 from mysafesqlColors import *
 
 def CheckSystem():
 
-    try:
-        with open('/etc/redhat-release', 'rb', 0) as file, mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-            if s.find(b'CentOS') == -1:
-                isCentOS = True
-            if s.find(b'Ubuntu') == -1:
-                isUbuntu
-            if s.find(b'CentOS') != -1 or s.find(b'Ubuntu'):
-                isNotSupported = True
-    except:
-        printRed("Aborting, opertaing system not supported!")
+    if platform.system() == 'Linux':
+
+        if (os.name == "Darwin"):
+            printRed("OS NOT supported")
+            exit()
+
+        if (os.name == "CentOS"):
+            printGreen("CentOS found...")
+# insert other possible OSes here
+# ...
+        else:
+            printRed("OS not supported")
+            exit()
+
+    else:
+        printRed("Currently only Linux platform is supported")
         exit()
 
-    if isNotSupported:
-        printRed("Aborting, opertaing system not supported!")
-        exit()
+    # try:
+    #     with open('/etc/redhat-release', 'rb', 0) as file, mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
+    #         if s.find(b'CentOS') == -1:
+    #             isCentOS = True
+    #         if s.find(b'Ubuntu') == -1:
+    #             isUbuntu
+    #         if s.find(b'CentOS') != -1 or s.find(b'Ubuntu'):
+    #             isNotSupported = True
+    # except:
+    #     printRed("Aborting, opertaing system not supported!")
+    #     exit()
 
-    if isCentOS:
-        printGreen("CentOS found...")
+    # if isNotSupported:
+    #     printRed("Aborting, opertaing system not supported!")
+    #     exit()
 
-    if isUbuntu:
-        printGreen("Ubuntu found...")
+    # if isCentOS:
+    #     printGreen("CentOS found...")
+
+    # if isUbuntu:
+    #     printGreen("Ubuntu found...")
