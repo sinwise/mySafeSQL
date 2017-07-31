e064781a590dbd2e2cb44ce4d0083b27e3ac1fe4
Sebastian Gogoasa <sebastian.gogoasa@sinwise.com>
2017-07-31 15:07:22 +0100

    Added PIGZ...

--- include/mysafesqlCheck.py
+++ include/mysafesqlCheck.py
@@ -9,7 +9,7 @@
 ##
 ##
 
-import imp, sys, os, platform
+import imp, sys, platform
 sys.path.append('')
 from mysafesqlColors import *
 
@@ -19,21 +19,56 @@ global isCentOS, isUbuntu
 def CheckSystem():
 
     if sys.platform.startswith('linux'):
+
         distro, version, dist_id = platform.linux_distribution()
+        
         if "CentOS" in distro:
             isCentOS = True
+        
         elif "Ubuntu" in distro:
             isUbuntu = True
+        
         else:
             printRed("Aborting, your operating system is not currenly supported.")
+    
     elif sys.platform.startswith('darwin'):
         printRed("Aborting, Mac OS is not currenly supported.")
         exit()
+    
     elif sys.platform.startswith('openbsd'):
         printRed("Aborting, OpenBSD is not currenly supported.")
         exit()
 
     if isCentOS:
+        
         printGreen("CentOS found on your server, checking packages...")
+        
+        import yum
+        yb          = yum.YumBase()
+        searchlist  = ['name']
+        arg         = ['pigz']
+        matches     = yb.searchGenerator(searchlist,arg)
+        
+        for (package, matched_value) in matches:
+        
+            if package.name == 'pigz':
+                yb.install(package)
+                yb.buildTransaction()
+                yb.processTransaction()
+
     elif isUbuntu:
+
         printGreen("Ubuntu found on your server, checking packages...")
+
+        import apt
+        ag          = apt.AptBase()
+        searchlist  = ['name']
+        arg         = ['pigz']
+        matches     = ap.searchGenerator(searchlist,arg)
+        
+        for (package, matched_value) in matches:
+        
+            if package.name == 'pigz':
+                ap.install(package)
+                ap.buildTransaction()
+                ap.processTransaction()
