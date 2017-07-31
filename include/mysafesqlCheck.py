d5cfe0b8ec5e3a0f35be8a13ec6ff0305d91dfb3
Sebastian Gogoasa <sebastian.gogoasa@sinwise.com>
2017-07-31 15:26:33 +0100

    OS Testing...

--- include/mysafesqlCheck.py
+++ include/mysafesqlCheck.py
@@ -42,19 +42,36 @@ def CheckSystem():
     if isCentOS:
         
         printGreen("CentOS found on your server, checking packages...")
-        
+
         import yum
-        yb          = yum.YumBase()
-        searchlist  = ['name']
-        arg         = ['pigz']
-        matches     = yb.searchGenerator(searchlist,arg)
-        
-        for (package) in matches:
-        
-            if package.name == 'pigz':
+
+        yb.conf.cache = 1 # enable cache
+        installed = yb.rpmdb.returnPackages()
+
+        packages = ['pigz']
+        for package in packages:
+            if package in installed:
+                print('{0} is already installed, skipping...'.format(package))
+            else:
+                print('Installing {0}'.format(package))
                 yb.install(package)
+                yb.resolveDeps()
                 yb.buildTransaction()
+
                 yb.processTransaction()
+        
+        # import yum
+        # yb          = yum.YumBase()
+        # searchlist  = ['name']
+        # arg         = ['pigz']
+        # matches     = yb.searchGenerator(searchlist,arg)
+        
+        # for (package, matched_value) in matches:
+        
+        #     if package.name == 'pigz':
+        #         yb.install(package)
+        #         yb.buildTransaction()
+        #         yb.processTransaction()
 
     elif isUbuntu:
 
